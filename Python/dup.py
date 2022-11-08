a50aab0aff1535fc4ce81c5b78bb6dbeae96e723
Samik Some <samiksome92@gmail.com>
2022-11-09 03:01:56 +0530

    Fix variable name.

--- Python/dup.py
+++ Python/dup.py
@@ -74,7 +74,7 @@ def find_dups(dirs: List[str], recursive: bool = False, no_hash: bool = False) -
                 dir_files += [os.path.join(r, f) for f in fs]
             files += sorted(dir_files)
         else:
-            files += sorted([os.path.join(dir, f) for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))])
+            files += sorted([os.path.join(dir_, f) for f in os.listdir(dir_) if os.path.isfile(os.path.join(dir_, f))])
     print(f'Found {len(files)} files across directories.')
 
     # Check file sizes.
