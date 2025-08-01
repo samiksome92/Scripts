1bbf97c3cfa73cbca2c4c6283c8b0042cf1450ee
Samik Some <samiksome92@gmail.com>
2025-08-02 03:33:49 +0530

    Add metadata block allow custom port, serve manually instead of calling undocumented test function and only use o-cache header.

--- Python/nocache.http.server.py
+++ Python/nocache.http.server.py
@@ -1,27 +1,50 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = ">=3.12"
+# ///
 
-"""A script implementing a simple http server with custom cache control headers."""
+"""A script implementing a simple http server with custom `Cache-Control` headers."""
 
+import argparse
 import http.server
-from typing import Union
+import sys
 
 
 class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     """Request handler for no cache server.
 
-    Extends SimpleHTTPRequestHandler and adds no-cache and must-revalidate Cache-Control headers to responses.
+    Extends `SimpleHTTPRequestHandler` and adds `no-cache` `Cache-Control` headers to responses.
     """
 
-    def send_response_only(self, code: int, message: Union[str, None] = None) -> None:
-        """Add Cache-Control headers and Expires header."""
+    def send_response_only(self, code: int, message: str | None = None) -> None:
+        """Sends response with `no-cache` `Cache-Control` headers.
+
+        Parameters
+        ----------
+        code : int
+            Response code.
+        message : str | None
+            Response message. (default=`None`)
+        """
         super().send_response_only(code, message=message)
-        self.send_header("Cache-Control", "no-store, must-revalidate")
-        self.send_header("Expires", "0")
+        self.send_header("Cache-Control", "no-cache")
 
 
 def main() -> None:
-    """Main function for script. Tests the NoCacheHTTPRequestHandler class."""
-    http.server.test(HandlerClass=NoCacheHTTPRequestHandler, bind="127.0.0.1", port=8000)
+    """Parses arguments and runs the server."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", help="Port to use", type=int, default=8000)
+    args = parser.parse_args()
+
+    NoCacheHTTPRequestHandler.protocol_version = "HTTP/1.1"
+    with http.server.ThreadingHTTPServer(("127.0.0.1", args.port), NoCacheHTTPRequestHandler) as httpd:
+        print(f"Serving HTTP on 127.0.0.1 port {args.port} (http://127.0.0.1:{args.port}/) ...")
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received, exiting.")
+            sys.exit(0)
 
 
 if __name__ == "__main__":
